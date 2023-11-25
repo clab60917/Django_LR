258ff2b972f8e3c2ea2f6f4b59e20c2301e421fd
Clément <clab60917@eleve.isep.fr>
2023-11-25 20:01:47 +0100

    test CI 1

--- django_backend/users/views.py
+++ django_backend/users/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'users/home.html')
+
+def register(request):
+    if request.methode == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hello {username}, your account was created successfully')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+        
+
+    
+    return render(request, 'users/register.html', {'form':form})
